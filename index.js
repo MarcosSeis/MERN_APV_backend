de9ada4f8bb1101c1b025703bd80d441c0cdb4dd
Marcos <marsanmar6291@gmail.com>
2023-01-19 08:59:43 -0600

    deployment

---
+++ index.js
@@ -0,0 +1,40 @@
+import  express  from "express";
+import dotenv from  "dotenv/config";
+import cors from "cors";
+import conectarDB from "./config/db.js";
+import mongoose from "mongoose";
+import veterinarioRoutes from "./routes/veterinarioRoutes.js";
+import pacienteRoutes from "./routes/pacienteRoutes.js"
+
+const app = express();
+app.use(express.json());
+
+mongoose.set('strictQuery', false);
+
+conectarDB();
+
+const dominiosPermitidos = [process.env.FRONTEND_URL];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (dominiosPermitidos.indexOf(origin) !== -1) {
+      // El Origen del Request esta permitido
+      callback(null, true);
+    } else {
+      callback(new Error("No permitido por CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+
+
+
+app.use('/api/veterinarios', veterinarioRoutes);
+app.use('/api/pacientes', pacienteRoutes);
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Servidor funcionando en el puerto ${PORT}`);
+});
+
